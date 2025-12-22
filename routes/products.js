d4a028715eaf880fad4f554f838cc5193a4c0915
orzion <skhn7412@gmail.com>
2025-12-22 20:02:35 +0200

    adding products post route and adimin json for tast

--- routes/products.js
+++ routes/products.js
@@ -12,15 +12,59 @@ router.get("/" , (req,res)=>{
     try {
         const data = fs.readFileSync(fileWithPath , "utf-8");
         if(!data){
-            return res.send("No Products in file")
+            return res.status(404).send("No Products in file");
         }
         const products = JSON.parse(data);
-        res.json(products);
+        res.status(200).json(products);
 
     } catch (error) {
         res.send([]);
     }
 
+});
+
+router.get("/:id", (req,res)=>{
+    const idFromParams = Number(req.params.id);
+    const data = JSON.parse(fs.readFileSync(fileWithPath , "utf-8"));
+    const index = data.findIndex(p => p.id === idFromParams);
+    if (index !== -1) {
+       return res .status(200).send(index);
+    }
+    
+    res.status(404).send("data not found");
+});
+
+const validateAdmin = (req, res, next) => {
+    const fileWithPath2 = path.join(__dirname, "admin.json");
+    const data = JSON.parse(fs.readFileSync(fileWithPath2, "utf-8"));
+
+    const adminId = req.body.id; 
+    const findAdmin = data.findIndex(p => p.id === adminId);
+
+    if (findAdmin === -1) {
+        return res.status(401).send("You are not admin, no permission");
+    }
+
+    next();
+};
+
+router.post('/', validateAdmin, (req, res) => {
+    const id = req.body.id;
+    console.log("BODY:", req.body);
+    const data = JSON.parse(fs.readFileSync(fileWithPath, "utf-8"));
+
+    const index = data.findIndex(p => p.id === id);
+
+    if (index !== -1) {
+        return res.status(409).send('Error: product already exists');
+    }
+
+    const newProduct = req.body;
+    data.push(newProduct);
+
+    fs.writeFileSync(fileWithPath, JSON.stringify(data));
+
+    res.status(201).json("New product was added successfully");
 });
 
 module.exports =  router;
